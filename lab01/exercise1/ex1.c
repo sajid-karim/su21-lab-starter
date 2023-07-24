e8928793f46819fe049776fcf390c132db5f1484
sajid-karim <22100253@lums.edu.pk>
2023-07-24 12:45:52 +0500

    Done exe1

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -7,7 +7,17 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+    int srt_len = strlen(str);
+    int count = 0;
+
+    for (int i = 0; i < srt_len; i++)
+    {
+        /* code */
+        if(str[i] == letter)
+            count++;
+    }
+    
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
@@ -15,5 +25,42 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    int i = 0;
+
+    while (dna_seq->sequence[i] != '\0')
+    {   
+        int count = 0;
+
+        switch (dna_seq->sequence[i])
+        {
+            case 'A':
+                /* code */
+                count = num_occurrences(dna_seq->sequence, dna_seq->sequence[i]);
+                printf("%d \n", count);
+                dna_seq->A_count = count;
+                break;
+            case 'C':
+                /* code */
+                count = num_occurrences(dna_seq->sequence, dna_seq->sequence[i]);
+                dna_seq->C_count = count;
+                break;
+            case 'G':
+                /* code */
+                count = num_occurrences(dna_seq->sequence, dna_seq->sequence[i]);
+                dna_seq->G_count = count;
+                break;
+            case 'T':
+                /* code */
+                count = num_occurrences(dna_seq->sequence, dna_seq->sequence[i]);
+                dna_seq->T_count = count;
+                break;
+            
+            
+            default:
+                printf("Wrong nucleotide /n");
+                break;
+        }
+        i++;
+    }
     return;
 }
