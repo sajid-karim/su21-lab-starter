afcea3ac3c51f7be584d8062bb84309f246823e2
sajid-karim <22100253@lums.edu.pk>
2023-07-25 10:27:28 +0500

    Complete exercise 4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,24 @@
-#include <stddef.h>
+// Copyright 2023 Sajid karim"
 #include "ll_cycle.h"
 
+#include <stddef.h>
+
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+  /* TODO: Implement ll_has_cycle */
+  node *fast_ptr = head;
+  node *slow_ptr = head;
+  while (fast_ptr != NULL) {
+    slow_ptr = slow_ptr->next;
+
+    if (fast_ptr->next == NULL) {
+      return 0;
+    }
+    fast_ptr = fast_ptr->next->next;
+
+    if (slow_ptr == fast_ptr) {
+      return 1;
+    }
+  }
+
+  return 0;
 }
